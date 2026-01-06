0112c5b37f812709c46b66c5c6962d8bbb7712cb
dallinnd <156145316+dallinnd@users.noreply.github.com>
2026-01-05 17:28:48 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,10 @@
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open("data-view-v1").then(cache =>
-      cache.addAll([
-        "./",
-        "./index.html",
-        "./style.css"
-      ])
-    )
-  );
+const CACHE_NAME = 'dataview-v1';
+const ASSETS = ['./', './index.html', './style.css', './app.js'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
-  );
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
